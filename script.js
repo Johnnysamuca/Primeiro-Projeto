7bc7ec11130f76ea4d12c4b7db573225421a3612
Samuel <johnnysamuel486@gmail.com>
2023-08-15 15:15:36 -0300

    :tada:initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function togglemode () {
+   const html = document.documentElement
+   html.classList.toggle('light')
+   const img = document.querySelector("#foto img")
+   
+   if(html.classList.contains('light')) {
+      img. src="./asset/assets/avatar-light.png"
+
+   }else {
+      img. src="./asset/assets/avatar.png"
+   }
+  
+
+}
+
+
+
+
+

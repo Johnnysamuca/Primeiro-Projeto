c69d6d95348b447afd8a18f10826c46a0d71146c
Samuel <johnnysamuel486@gmail.com>
2023-08-15 22:19:41 -0300

    troca da foto de perfil e do @

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function togglemode () {
    const html = document.documentElement
    html.classList.toggle('light')
-   const img = document.querySelector("#foto img")
+   //const img = document.querySelector("#foto img")
    
-   if(html.classList.contains('light')) {
-      img. src="./asset/assets/avatar-light.png"
+  // if(html.classList.contains('light')) {
+     // img. src="./asset/assets/avatar-light.png"
 
-   }else {
-      img. src="./asset/assets/avatar.png"
-   }
+  // }else {
+     // img. src="./asset/assets/avatar.png"
+   //}
   
 
 }

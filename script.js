83c20a032b58aaa8fc3e4f156140acf4519a4079
Samuel Johnny <johnnysamuel486@gmail.com>
2024-09-19 23:52:08 -0300

    alteração em link do intagram

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-function togglemode () {
-   const html = document.documentElement
-   html.classList.toggle('light')
-   const img = document.querySelector("#foto img")
-   
-   if(html.classList.contains('light')) {
-      img. src="./asset/assets/avatar-light-johnny..png"
+function togglemode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+  const img = document.querySelector("#foto img");
 
-   }else {
-      img. src="./asset/assets/avatar-johnny.png"
-   }
-  
-
+  if (html.classList.contains("light")) {
+    img.src = "./asset/assets/avatar-light-johnny..png";
+  } else {
+    img.src = "./asset/assets/avatar-johnny.png";
+  }
 }
-
-
-
-
-
